584dcce84f0a1eb59cee68e90f0e49232d8e175e
Umang-19 <umangaggarwal537@gmail.com>
2024-02-19 22:04:15 +0530

    Routing and Header for Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './Header';
 
 const Login = () =>{
     return (
         <>
-        <div>Log In</div>
+        <Header />
+        <div>
+            <img className='' src='https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='background' />
+        </div>
         </>
     )
 }
