efd410079b06f0b026bb7b8401db3ec406fb73c3
Umang-19 <umangaggarwal537@gmail.com>
2024-02-24 18:42:10 +0530

    Sign out

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { signOut } from 'firebase/auth';
+import { auth } from '../utils/firebase';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { removeUser } from '../utils/userSlice';
 
 const Header = () => {
+
+  const navigate = useNavigate();
+
+  const handleSignOutButton = () =>{
+    signOut(auth).then(() => {
+      navigate("/");
+    }).catch((error) => {
+      navigate("/error");
+    });
+  }
+
   return (
-    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10'>
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between'>
         <img className="w-44" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
-     
+    
+    <div className='flex p-2'>
+      <img className='w-12 h-12' alt="usericon" src="https://occ-0-1492-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" />
+      <button className='font-bold text-white' onClick={handleSignOutButton}>(Sign Out)</button>
+    </div>
     </div>
   )
 }
