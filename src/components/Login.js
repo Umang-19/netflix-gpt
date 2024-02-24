820c9cf1fb560319f4579ee40480e23015709afd
Umang-19 <umangaggarwal537@gmail.com>
2024-02-24 15:32:58 +0530

    Firebase Authentication Logic

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
 import React, { useRef, useState } from 'react'
 import Header from './Header';
 import { checkValidData } from '../utils/validate';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../utils/firebase';
 
 
-const Login = () =>{
+const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null);
@@ -11,36 +14,71 @@ const Login = () =>{
     const email = useRef(null);
     const password = useRef(null);
 
-    const toggleSignInForm = () =>{
+
+
+    const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
     }
 
-    const handleButtonClick = () =>{
+    const handleButtonClick = () => {
         // Validate form data
         const message = checkValidData(email.current.value, password.current.value);
+        console.log(password.current.value);
         setErrorMessage(message);
+
+        if (message) return;
+
+        if (!isSignInForm) {
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    // Signed up 
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + " - " + errorMessage);
+                });
+        }
+        else {
+            // Sign In
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    // Signed in 
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log(errorCode + " - " + errorMessage);
+                    setErrorMessage("Invalid Credentials!");
+                });
+        }
+
     }
 
-    return ( 
+    return (
         <>
-        <Header />
-        <div className='absolute'>
-            <img className='' src='https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='background' />
-        </div>
-        <form onSubmit={(e) => e.preventDefault()} className='absolute p-12 bg-black w-3/12 my-40 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg'>
-            <h1 className='font-bold text-3xl py-4'>{isSignInForm? "Sign In" : "Sign Up"}</h1>
-            { !isSignInForm && <input type='text' placeholder='Full Name' className='p-4 my-2 w-full bg-gray-800 rounded-md' /> }
+            <Header />
+            <div className='absolute'>
+                <img className='' src='https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877f-45ba7a60c16f/2642e08e-4202-490e-8e93-aff04881ee8a/IN-en-20240212-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='background' />
+            </div>
+            <form onSubmit={(e) => e.preventDefault()} className='absolute p-12 bg-black w-3/12 my-40 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg'>
+                <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+                {!isSignInForm && <input type='text' placeholder='Full Name' className='p-4 my-2 w-full bg-gray-800 rounded-md' />}
 
-            <input type='text' ref={email} placeholder='Email Address' className='p-4 my-2 w-full bg-gray-800 rounded-md' />
-            <input type='password' ref={password} placeholder='Password' className='p-4 my-2 w-full bg-gray-800  rounded-md' />
-            
-            <p className='py-2 text-red-500 font-bold text-lg'>{errorMessage}</p>
+                <input type='text' ref={email} placeholder='Email Address' className='p-4 my-2 w-full bg-gray-800 rounded-md' />
+                <input type='password' ref={password} placeholder='Password' className='p-4 my-2 w-full bg-gray-800  rounded-md' />
 
-            <button className='p-4 my-10 bg-red-700 w-full  rounded-md' onClick={handleButtonClick}>{isSignInForm? "Sign In" : "Sign Up"}</button>
-            <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
-            {isSignInForm? "New to Netflix? Sign Up Now" : "User Already Exists! Sign In"}
-            </p>
-        </form>
+                <p className='py-2 text-red-500 font-bold text-lg'>{errorMessage}</p>
+
+                <button className='p-4 my-10 bg-red-700 w-full  rounded-md' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+                <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>
+                    {isSignInForm ? "New to Netflix? Sign Up Now" : "User Already Exists! Sign In"}
+                </p>
+            </form>
         </>
     )
 }
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,7 +2,7 @@
 export const checkValidData = (email, password) =>{
 
     const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
-    const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/.test(password);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}/.test(password);
 
     if(!isEmailValid) return "Email Address is not valid!";
     if(!isPasswordValid) return "Password is not valid!";
