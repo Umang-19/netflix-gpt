2a69f364eec03d86aa2f2b180f7389236c6e2768
Umang-19 <umangaggarwal537@gmail.com>
2024-02-25 20:43:15 +0530

    Restricting /browse for not logged in

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,7 +26,6 @@ const Login = () => {
         }).then(() => {
             const {uid, email, displayName, photoURL} = auth.currentUser;
             dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
-            navigate("/browse");
         }).catch((error) => {
             // An error occurred
             // ...
@@ -67,7 +66,6 @@ const Login = () => {
                     // Signed in 
                     const user = userCredential.user;
                     console.log(user);
-                    navigate("/browse");
                 })
                 .catch((error) => {
                     const errorCode = error.code;
