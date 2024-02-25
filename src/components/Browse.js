3ffb1505bd71bb4a6699d4441792e514d327c229
Umang-19 <umangaggarwal537@gmail.com>
2024-02-25 22:08:57 +0530

    Adding TMDB API and movieSlice

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
+import { API_OPTIONS, GET_NOW_PLAYING_MOVIES_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/movieSlice';
 
 const Browse = () => {
+
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () =>{
+    const data = await fetch(GET_NOW_PLAYING_MOVIES_URL, API_OPTIONS)
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addNowPlayingMovies(json.results));
+  };
+
+  useEffect(()=>{
+    getNowPlayingMovies();  
+  }, []);
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,12 @@
 export const NETFLIX_LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const BLUE_AVATAR = "https://occ-0-1492-3662.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTUyYTllYjg0YThhOGY4ZTk0NGY3ODdkMWEyMDU3YSIsInN1YiI6IjY1ZGI2NWNlMTc1MDUxMDE4N2Y5ZTBjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xSYYLiCEJVimsyVjXGq6rfxkdIEbuLm0Im6A1S0VMM0'
+    }
+};
+
+export const GET_NOW_PLAYING_MOVIES_URL = "https://api.themoviedb.org/3/movie/now_playing?page=1"; 
